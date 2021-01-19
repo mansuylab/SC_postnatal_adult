b5f75b26133b133db7e4b047f11b77729cd758e4
Deepak Tanwar <dktanwar@hotmail.com>
2021-01-19 11:04:21 +0100

    ms: fig v4 #41

--- exp/20201103-manuscript_figs_v4-41/bin/functions.R
+++ exp/20201103-manuscript_figs_v4-41/bin/functions.R
@@ -25,7 +25,7 @@ ht_opt$message <- FALSE
 
 
 # Colors ----
-cols <- rev(plasma(n = 10))[c(2,4,6,8,10)]
+cols <- rev(plasma(n = 10))[c(2, 4, 6, 8, 10)]
 cols <- c(PND8 = cols[1], PND15 = cols[3], PND14 = cols[2], PNW8 = cols[4], Adult = cols[5])
 
 
@@ -34,83 +34,84 @@ myPalette <- colorRampPalette(colors = viridis::plasma(n = 7)[c(2, 4, 6)])
 
 
 # Theme ----
-th <- theme_bw(base_family = "Helvetica") + 
+th <- theme_bw(base_family = "Helvetica") +
   theme(
-  legend.background = element_rect(),
-  
-  # title, subtitle, and caption
-  plot.title = element_text(
-    colour = "black",
-    angle = 0,
-    size = 10,
-    face = "bold",
-    vjust = 1
-  ),
-  plot.subtitle = element_text(
-    colour = "black",
-    angle = 0,
-    size = 10,
-    face = "plain",
-    vjust = 1
-  ),
-  plot.caption = element_text(
-    colour = "black",
-    angle = 0,
-    size = 10,
-    face = "plain",
-    vjust = 1
-  ),
-  
-  # axis text
-  axis.text.x = element_text(
-    colour = "black",
-    angle = 0,
-    size = 9,
-    # vjust = 0.5,
-    # hjust = 0.5
-  ),
-  axis.text.y = element_text(
-    colour = "black",
-    angle = 0,
-    size = 9,
-    # vjust = 0.5,
-    # hjust = 0.5
-  ),
-  axis.title.x = element_text(
-    colour = "black",
-    size = 10,
-    face = "plain"
-  ),
-  axis.title.y = element_text(
-    colour = "black",
-    size = 10,
-    face = "plain"
-  ),
-  
-  # legend
-  legend.position = "top",
-  legend.key = element_blank(),
-  # legend.key.size = unit(0, "cm"),
-  legend.text = element_text(
-    colour = "black",
-    size = 9,
-    margin = margin(r = 0, t = 0, b = 0, l = 0, unit = "pt"),
-    vjust = 0, hjust = 0
-  ),
-  legend.title = element_text(size = 10, face = "plain", colour = "black"),
-  legend.justification = "bottom",
-  legend.margin = margin(c(0, 0, 0, 0)),
-  legend.box.margin = margin(0, 0, -7, 0),
-  legend.key.height = unit(0, "cm"),
-  legend.key.width = unit(0, "cm"),
-  legend.spacing = unit(1, "mm"),
-  plot.margin = grid::unit(c(0, 0, 0, 0), "mm"),
-  strip.background = element_rect(fill = NA, colour = "black"),
-  strip.text.x = element_text(face = "plain", colour = "black", size = 9)
-) +
+    legend.background = element_rect(),
+
+    # title, subtitle, and caption
+    plot.title = element_text(
+      colour = "black",
+      angle = 0,
+      size = 10,
+      face = "bold",
+      vjust = 1
+    ),
+    plot.subtitle = element_text(
+      colour = "black",
+      angle = 0,
+      size = 10,
+      face = "plain",
+      vjust = 1
+    ),
+    plot.caption = element_text(
+      colour = "black",
+      angle = 0,
+      size = 10,
+      face = "plain",
+      vjust = 1,
+      hjust = 0.5
+    ),
+
+    # axis text
+    axis.text.x = element_text(
+      colour = "black",
+      angle = 0,
+      size = 9,
+      # vjust = 0.5,
+      # hjust = 0.5
+    ),
+    axis.text.y = element_text(
+      colour = "black",
+      angle = 0,
+      size = 9,
+      # vjust = 0.5,
+      # hjust = 0.5
+    ),
+    axis.title.x = element_text(
+      colour = "black",
+      size = 10,
+      face = "plain"
+    ),
+    axis.title.y = element_text(
+      colour = "black",
+      size = 10,
+      face = "plain"
+    ),
+
+    # legend
+    legend.position = "top",
+    legend.key = element_blank(),
+    # legend.key.size = unit(0, "cm"),
+    legend.text = element_text(
+      colour = "black",
+      size = 9,
+      margin = margin(r = 0, t = 0, b = 2, l = 0, unit = "pt"),
+      vjust = 0, hjust = 0
+    ),
+    legend.title = element_text(size = 10, face = "plain", colour = "black"),
+    legend.justification = "bottom",
+    legend.margin = margin(c(0, 0, 0, 0)),
+    legend.box.margin = margin(0, 0, -7, 0),
+    legend.key.height = unit(0, "cm"),
+    legend.key.width = unit(0, "cm"),
+    legend.spacing = unit(1, "mm"),
+    plot.margin = grid::unit(c(0, 10, 0, 0), "mm"),
+    strip.background = element_rect(fill = NA, colour = "black"),
+    strip.text.x = element_text(face = "plain", colour = "black", size = 10)
+  ) +
   theme(
     panel.background = element_rect(fill = "white"),
-    panel.border = element_rect(colour = "black", fill=NA, size=0.7),
+    panel.border = element_rect(colour = "black", fill = NA, size = 0.7),
     strip.background = element_rect(color = "black", size = 0.7)
   )
 
@@ -119,4 +120,48 @@ hm_cols <- function(x, col = viridis(3)) {
   q <- quantile(data.matrix(x), c(0.01, 0.99))
   ramp <- colorRamp2(c(min(q), 0, max(q)), col)
   return(ramp)
+}
+
+
+#' Title
+#'
+#' @param x A vector of strings
+#' @param minSizeForBreak minimum break size
+#' @param lb length break
+#' @param nb number of breaks
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+breakStrings <- function(x, minSizeForBreak = 20, lb = "\n", nb = 2) {
+  sapply(x, minSizeForBreak = minSizeForBreak, lb = lb, FUN = function(x,minSizeForBreak, lb) {
+    if (nchar(x) <= minSizeForBreak) {
+      return(x)
+    }
+    
+    g <- gregexpr(" ", x)[[1]]
+    if (length(g) == 0) {
+      return(x)
+    }
+    if (length(g) == 1 & all(g == -1)) {
+      return(x)
+    }
+    
+    if(nb == 2){
+      mid <- nchar(x) / 2
+      mid <- g[order(abs(g - mid))[1]]
+      substr(x, mid, mid) <- lb
+    } else if(nb == 3){
+      mid1 <- round(nchar(x) / 3)
+      mid2 <- mid1*2
+      mid1 <- g[order(abs(g - mid1))[1]]
+      mid2 <- g[order(abs(g - mid2))[1]]
+      substr(x, mid1, mid1) <- lb
+      substr(x, mid2, mid2) <- lb
+    }
+    
+    return(x)
+  })
 }
